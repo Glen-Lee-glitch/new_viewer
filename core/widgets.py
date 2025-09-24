73ae0fa45b696763b57912a993c70e985959e5be
Glen Lee <dlrudrn2088@gmail.com>
2025-09-24 16:20:54 +0900

    feat: ZoomableGraphicsView 클래스 추가 및 PDF 뷰어에서 사용 - Ctrl + 마우스 휠로 확대/축소 기능 구현 - QGraphicsView를 상속받아 ZoomableGraphicsView로 변경

--- core/widgets.py
+++ core/widgets.py
@@ -11,6 +11,32 @@ from PyQt6.QtGui import QPixmap, QPainter, QIcon
 from PyQt6 import uic
 from .pdf_render import PdfRender
 
+class ZoomableGraphicsView(QGraphicsView):
+    """Ctrl + 마우스 휠로 확대/축소가 가능한 QGraphicsView"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag) # 마우스로 끌어서 이동
+
+    def wheelEvent(self, event):
+        """마우스 휠 이벤트를 재정의하여 확대/축소 기능 구현"""
+        if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
+            zoom_in_factor = 1.15
+            zoom_out_factor = 1 / zoom_in_factor
+
+            if event.angleDelta().y() > 0:
+                zoom_factor = zoom_in_factor
+            else:
+                zoom_factor = zoom_out_factor
+            
+            self.scale(zoom_factor, zoom_factor)
+        else:
+            # Ctrl 키가 눌리지 않으면 기본 스크롤 동작을 수행
+            super().wheelEvent(event)
+
 class PdfLoadWidget(QWidget):
     """PDF 로드 영역 위젯"""
     pdf_selected = pyqtSignal(str)  # PDF 파일 경로를 전달하는 시그널
