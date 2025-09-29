0fcfe41149478bbfbfdd3c821423676d00b24c25
Glen Lee <dlrudrn2088@gmail.com>
2025-09-29 10:17:25 +0900

    feat(view): ZoomableGraphicsView에 Shift+휠 수평 스크롤 기능 추가
    - `ZoomableGraphicsView`에서 Shift 키를 누른 상태로 마우스 휠을 움직이면 수평으로 스크롤되도록 `wheelEvent` 수정
    - 확대/축소 시 필요에 따라 스크롤바가 표시되도록 스크롤바 정책을 `ScrollBarAsNeeded`로 변경

--- widgets/zoomable_graphics_view.py
+++ widgets/zoomable_graphics_view.py
@@ -4,16 +4,16 @@ from PyQt6.QtWidgets import QGraphicsView
 
 
 class ZoomableGraphicsView(QGraphicsView):
-    """Ctrl + 마우스 휠로 확대/축소가 가능한 QGraphicsView"""
+    """Ctrl + 마우스 휠로 확대/축소, Shift + 휠로 수평 스크롤이 가능한 QGraphicsView"""
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
         self.setResizeAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
 
     def wheelEvent(self, event):
-        """마우스 휠 이벤트를 재정의하여 확대/축소 기능 구현"""
+        """마우스 휠 이벤트를 재정의하여 확대/축소 및 수평 스크롤 기능 구현"""
         if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
             zoom_in_factor = 1.15
             zoom_out_factor = 1 / zoom_in_factor
@@ -24,6 +24,10 @@ class ZoomableGraphicsView(QGraphicsView):
                 zoom_factor = zoom_out_factor
             
             self.scale(zoom_factor, zoom_factor)
+        elif event.modifiers() == Qt.KeyboardModifier.ShiftModifier:
+            # Shift 키와 함께 휠을 돌리면 수평 스크롤
+            h_bar = self.horizontalScrollBar()
+            h_bar.setValue(h_bar.value() - event.angleDelta().y())
         else:
             super().wheelEvent(event)
 
