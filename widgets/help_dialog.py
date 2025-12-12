8ddf651ab84fa095856fc5eceffa8b1eb14b5dfc
Glen Lee <dlrudrn2088@gmail.com>
2025-12-12 10:17:07 +0900

    feat: 도움말 다이얼로그 content_container 레이아웃 구성

--- widgets/help_dialog.py
+++ widgets/help_dialog.py
@@ -26,4 +26,23 @@ class HelpDialog(QDialog):
         # Splitter 비율 설정 (col1:col2 = 2:8)
         self.splitter.setStretchFactor(0, 2)
         self.splitter.setStretchFactor(1, 8)
+        
+        # content_layout 비율 설정 (이미지:텍스트 = 7:3)
+        self.content_layout.setStretch(0, 7)
+        self.content_layout.setStretch(1, 3)
+        
+        # 리스트 아이템 선택 시그널 연결
+        self.listWidget.currentItemChanged.connect(self._on_item_selected)
+        
+        # 초기 선택 시 내용 표시
+        if self.listWidget.count() > 0:
+            self.listWidget.setCurrentRow(0)
+    
+    def _on_item_selected(self, current, previous):
+        """리스트 아이템이 선택되었을 때 호출되는 메서드"""
+        if current is None:
+            return
+        
+        # 임시 텍스트 업데이트
+        self.text_label.setText("이건 임시 텍스트입니다.")
 
